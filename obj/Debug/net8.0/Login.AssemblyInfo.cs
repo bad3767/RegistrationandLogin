86a48e6e10fda0264ad27a286d9fb16b742d5c95
magesh <bad376788@gmail.com>
2025-03-20 10:31:12 +0530

    adding gitignore

--- obj/Debug/net8.0/Login.AssemblyInfo.cs
+++ obj/Debug/net8.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a55368f295f855e7cfb5ba185a484b2d82c5ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a870bf72816d679d603c3845efc0d925db3ac20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
